595ad65f29f7f0bbc963aa534f8b04872f763d72
Joey <jvanhoek@hotmail.com>
2018-12-13 14:32:50 +0100

    Added shaders and a plane

--- main.js
+++ main.js
@@ -8,28 +8,62 @@
 		document.body.appendChild(renderer.domElement);
 		var numCubes = 10;
 
+
+
+		var shadervs = 
+		`
+			void main() {
+            gl_Position =  projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+        	}
+		`;
+
+		var shaderfs = 
+		`
+		 	uniform vec2 u_resolution;
+	        uniform float u_time;
+
+	        const float pps = 100.0; //pixes per second
+
+	        void main() {
+
+	        	float posy = gl_FragCoord.y + sin(u_time) * pps;
+	        	float posx = gl_FragCoord.x + u_time * -pps;
+
+	        	float color = mod(posy, u_resolution.y/2.0);
+
+	            vec2 st = gl_FragCoord.xy/u_resolution.xy;
+	            //gl_FragColor=vec4(0.5, 0.2, 0.2, 1.0);
+	            gl_FragColor=vec4(0.5* color, 0.2 * color, 0.2* color, 1.0);
+	        }
+		`;
+
+		    var uniforms = {
+                u_time: { type: "f", value: 1.0 },
+                u_resolution: { type: "v2", value: new THREE.Vector2() },
+                u_mouse: { type: "v2", value: new THREE.Vector2() }
+            };
+
+
+		    var planematerial = new THREE.ShaderMaterial( {
+                uniforms: uniforms,
+                vertexShader: shadervs,
+                fragmentShader: shaderfs
+            } );
+
 		//geometry
 		var cubeGeometry = new THREE.BoxGeometry(1, 1,1 );
-		var planeGeometry = new THREE.PlaneGeometry(4, 4, 1);
+		var planeGeometry = new THREE.PlaneGeometry(10, 10, 10);
 		var material = new THREE.MeshBasicMaterial( {color: 0x0c6297  });
 		var cube = new THREE.Mesh(cubeGeometry, material);
-		var plane = new THREE.Mesh(planeGeometry, material);
-		scene.add(cube);
-		scene.add(plane);
+		var plane = new THREE.Mesh(planeGeometry, planematerial);
 
-		// var planeMaterial;
-		// var loader = new THREE.FileLoader();
-		// loader.load('shaders/plane.vs',
-		// 		function(data){
-		// 			planeMaterial planeMaterial = new THREE.ShaderMaterial({
-		// 				vertexShader: loader.text;,
-		// 				fragmentShader: shaders/plane.fs
-		// 			});
 
-		// 	}
-		// );
 		//camera var
 		camera.position.z = 5;
+		plane.position.z = -2;
+
+		scene.add(cube);
+		scene.add(plane);
 
 		//cube list
 		var cubes =  new Array(numCubes);
@@ -53,6 +87,9 @@
 		function animate(){
 			requestAnimationFrame(animate);
 			resize();
+
+			uniforms.u_time.value = clock.getElapsedTime();
+			uniforms.u_resolution.value = new THREE.Vector2(canvas.clientWidth, canvas.clientHeight);
 
 			cubes.forEach(transform);
 
