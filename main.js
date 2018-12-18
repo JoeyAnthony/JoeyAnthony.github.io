5e8e58da91ecc1def0f8e872d45fb8631a1522a7
Joey <jvanhoek@hotmail.com>
2018-12-18 22:14:34 +0100

    Modified text

--- main.js
+++ main.js
@@ -40,7 +40,7 @@
     			vec3 w1 = wave(st, 1.3, 16.628, 15.946,-0.508,vec3(0.495,0.145,0.112), bg, u_time/1.);
     			vec3 w2 = wave(st, 1.3, 7.076, 5.874,7.508, vec3(0.530,0.100,0.120), bg, u_time/1.952);
     			vec3 w3 = wave(st, 1.3, 5.5, 1.378,5.364, vec3(0.173,0.230,0.740), bg, u_time/1.6);
-    			vec3 w4 = wave(st, 1.3, 5.5, 2.25,1.276, vec3(0.535,0.654,0.680), bg, sin(u_time));
+    			vec3 w4 = wave(st, 1.3, 5.5, 2.25,1.276, vec3(0.535,0.654,0.680), bg, u_time);
     			vec3 w5 = wave(st, 1.3, 5.5, 3.754,3.316, vec3(0.348,0.366,0.985), bg, sin(u_time-1.35));
 
 				
@@ -66,7 +66,7 @@
 				v_NormalDir = (modelMatrix * vec4( normal, 1.0 )).xyz;
 				v_CamPosition = cameraPosition;
 
-				gl_Position = projectionMatrix * viewMatrix * vec4(v_VertPosition, 1.0);
+				gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 			}
 		`;
 
@@ -87,7 +87,7 @@
 			vec3 lights[2];
 			vec3 lightColor = 	vec3(.9, 0.6, 0.3)/2.0;
 
-			//vec3 surface = 		vec3( .8, 0.2, 0.3);
+			//vec3 surface = 	vec3( .8, 0.2, 0.3);
 			vec3 cool = 		vec3( .0, .0, .55);
 			vec3 warm = 		vec3(0.5, 0.3, 0.0);
 			vec3 highlight =	vec3(2.0, 2.0, 2.0);
